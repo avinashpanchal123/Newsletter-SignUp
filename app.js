1a567e990dba4411d574db0d5e93abbf2a43490e
Avinash Panchal <88651615+avinashpanchal123@users.noreply.github.com>
2022-02-15 00:34:33 +0530

    able to submit the form data to "/" route

--- app.js
+++ app.js
@@ -1,16 +1,23 @@
 const express = require("express");
-// const bodyParser = require("body-parser");
-const request = require("request")
+const bodyParser = require("body-parser");
+const request = require("request");
 
 const app = express();
-// app.use(bodyParser.urlencoded({extended:true}))
-app.use(express.static("public"))
-app.use(express.json());
+app.use(bodyParser.urlencoded({extended:true}))
+app.use(express.static("public"));
+// app.use(express.json());
 
-app.get("/", (req, res)=>{
-    res.sendFile(__dirname + "/signUp.html")
-})
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/signUp.html");
+});
 
-app.listen(3000, (req, res)=>{
-    console.log("listening on port 3000");
-})
+app.post("/", (req, res) => {
+   let firstName = req.body.firstName;
+   let lastName  = req.body.lastName
+   let email = req.body.email;
+   console.log(firstName, lastName,  email);
+});
+
+app.listen(3000, (req, res) => {
+  console.log("listening on port 3000");
+});
